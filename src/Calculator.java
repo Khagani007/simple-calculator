7330e2452fc8794ea1430ad02e27eb2d1bec4db8
Khagani007 <khaganimammadli1999@gmail.com>
2023-11-21 11:48:05 +0400

    commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,37 @@
-public class Main {
+import java.util.Scanner;
+
+public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int operator;
+        double n1, n2;
+
+        System.out.println("1 - Add\n2 - Substract\n3 - Multiply\n4 - Divide");
+        System.out.println("Choose Operator : ");
+        Scanner scanner = new Scanner(System.in);
+        operator = scanner.nextInt();
+        System.out.println("Enter first number : ");
+        n1 = scanner.nextDouble();
+        System.out.println("Enter second number : ");
+        n2 = scanner.nextDouble();
+        scanner.close();
+        double result = 0;
+        switch (operator) {
+            case 1:
+                result = n1 + n2;
+                break;
+            case 2:
+                result = n1 - n2;
+                break;
+            case 3:
+                result = n1 * n2;
+                break;
+            case 4:
+                result = n1 / n2;
+                break;
+            default:
+                System.out.println("Entered operator is not valid");
+        }
+        System.out.println("Result : " + result);
+
     }
 }
